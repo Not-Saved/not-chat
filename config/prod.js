2782bbeca0a09f94a325d91d22c77eb382f8a9af
not-saved <corradin.loris@gmail.com>
2019-10-27 19:02:31 +0100

    Commit

--- config/prod.js
+++ config/prod.js
@@ -2,5 +2,7 @@ module.exports = {
 	google: {
 		clientID: process.env.GOOGLE_CLIENT_ID,
 		clientSecret: process.env.GOOGLE_CLIENT_SECRET
-	}
+	},
+	mongoURI: process.env.MONGO_URI,
+	cookieKey: process.env.COOKIE_SECRET
 };
