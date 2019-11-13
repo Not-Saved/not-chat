b2b48037bf44fd9b7a9b11386222fd7c1ee4619b
not-saved <corradin.loris@gmail.com>
2019-11-13 21:52:06 +0100

    Commit

--- middlewares/server/static.js
+++ middlewares/server/static.js
@@ -1,7 +1,11 @@
 const express = require("express");
+const path = require("path");
 
 module.exports = app => {
-	if (process.env.NODE_ENV === "production") {
-		app.use(express.static("client/public"));
-	}
+  if (process.env.NODE_ENV === "production") {
+    app.use(express.static("client/public"));
+    app.get("*", (req, res) => {
+      res.sendFile(path.join(__dirname, "client", "public", "404.html"));
+    });
+  }
 };
