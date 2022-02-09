107950ff5d3773539944d1b1c0eac6451844a349
not-saved <corradin.loris@outlook.com>
2022-02-09 15:58:03 +0100

    hack changed to better hack

--- client/src/components/chatScreen/messageList/message.js
+++ client/src/components/chatScreen/messageList/message.js
@@ -7,15 +7,7 @@ import replaceURLs from "../../../util/replaceURLs"
 const Message = ({ date, text, from, picture, mine, arrow, status }) => {
   const isMine = mine ? styles.right : ""
   const isArrow = arrow ? styles.arrow : ""
-  const textArray = text.split(/\n/).map(block => {
-    const wordArray = block.split(" ").map(e => `${e} `)
-    const arrayWithAnchorTags = replaceURLs(wordArray).flat()
-    return arrayWithAnchorTags
-      .map(e =>
-        e.length > 10 && typeof e === "string" ? e.match(/[\s\S]{1,10}/g) : e
-      )
-      .flat()
-  })
+  const textArray = replaceURLs(text.split(" "))
 
   return (
     <div className={[styles.container, isMine, isArrow].join(" ")} role="cell">
@@ -29,30 +21,19 @@ const Message = ({ date, text, from, picture, mine, arrow, status }) => {
           <div>
             <strong>{from}</strong>
           </div>
-          <div className={styles.contentContainer}>
-            {textArray.map((block, outerIndex) => (
-              <div key={outerIndex} className={styles.content}>
-                {block.map((block, innerIndex) => {
-                  return typeof block === "string" ? (
-                    <span key={innerIndex} className={styles.text}>
-                      {block}
-                    </span>
-                  ) : (
-                    block
-                  )
-                })}
-                {textArray.length === outerIndex + 1 && (
-                  <div className={styles.angle}>
-                    <div>{date}</div>
-                    {status === "PENDING" ? (
-                      <FaRegCircle className={styles.check} />
-                    ) : (
-                      <FaCheck className={styles.check} />
-                    )}
-                  </div>
+          <div>
+            <div className={styles.text}>{textArray}</div>
+            <div className={styles.angleContainer}>
+              <span>{date}</span>
+              <div className={styles.angle}>
+                <div>{date}</div>
+                {status === "PENDING" ? (
+                  <FaRegCircle className={styles.check} />
+                ) : (
+                  <FaCheck className={styles.check} />
                 )}
               </div>
-            ))}
+            </div>
           </div>
         </div>
       </div>
--- client/src/util/replaceURLs.js
+++ client/src/util/replaceURLs.js
@@ -4,20 +4,21 @@ const URLsRegex = /\b(http|https)?:\/\/\S+/gi
 
 export default function replaceURLs(stringArray) {
   const reconstructed = []
+  let textPart = ""
   stringArray.forEach((block, idx) => {
     if (block.match(URLsRegex)) {
-      const splittedLink = block
-        .substring(0, 200)
-        .match(/[\s\S]{1,10}/g)
-        .map((e, idx) => (
-          <a key={idx} href={block} rel="noopener noreferrer" target="_blank">
-            {e}
-          </a>
-        ))
-      reconstructed.push(splittedLink)
+      const link = (
+        <a href={block} rel="noopener noreferrer" target="_blank">
+          {block}
+        </a>
+      )
+      reconstructed.push(textPart + " ")
+      textPart = ""
+      reconstructed.push(link)
     } else {
-      return reconstructed.push(block)
+      textPart += `${idx === 0 ? "" : " "}${block}`
     }
   })
+  reconstructed.push(textPart)
   return reconstructed
 }
